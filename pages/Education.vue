b85262caf2b096ddc6b0c99c582a70d1405d2885
Davide-Z <davide_z@live.fr>
2019-08-15 12:15:26 +0200

    page education : - ENSAE logo updated - emoji added - timeline width adjusted

--- pages/Education.vue
+++ pages/Education.vue
@@ -1,50 +1,56 @@
 <template>
-  <v-timeline :dense="$vuetify.breakpoint.smAndDown">
-    <v-timeline-item v-for="(education, i) in educations" :key="i">
-      <v-card class="elevation-10">
-        <v-img :src="education.image" height="300" />
-        <v-card-title>
-          <div>
-            <div class="title">{{ education.school }}</div>
-            <span class="subtitle">
-              {{ education.degree }}
-            </span>
-            <span v-if="$vuetify.breakpoint.smAndDown">
-              ({{ education.year }})
-            </span>
-          </div>
-          <v-spacer></v-spacer>
-          <v-img
-            v-if="$vuetify.breakpoint.mdAndUp"
-            :src="education.logo"
-            height="100"
-            contain
-          />
-        </v-card-title>
-        <v-card-text>{{ education.description }}</v-card-text>
+  <v-layout justify-center>
+    <v-flex md10>
+      <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+        <v-timeline-item v-for="(education, i) in educations" :key="i">
+          <v-card class="elevation-20">
+            <v-img :src="education.image" height="200" />
+            <v-card-title>
+              <div>
+                <div class="title">{{ education.school }}</div>
+                <span class="subtitle">
+                  {{ education.degree }}
+                </span>
+                <span v-if="$vuetify.breakpoint.smAndDown">
+                  ({{ education.year }})
+                </span>
+              </div>
+              <v-spacer></v-spacer>
+              <v-img
+                v-if="$vuetify.breakpoint.mdAndUp"
+                :src="education.logo"
+                height="100"
+                contain
+              />
+            </v-card-title>
+            <v-card-text>{{ education.description }}</v-card-text>
 
-        <v-card-actions>
-          <v-btn flat @click="education.show = !education.show">
-            Courses
-            <v-icon>
-              {{ education.show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}
-            </v-icon>
-          </v-btn>
-        </v-card-actions>
+            <v-card-actions>
+              <v-btn flat @click="education.show = !education.show">
+                Courses
+                <v-icon>
+                  {{
+                    education.show ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+                  }}
+                </v-icon>
+              </v-btn>
+            </v-card-actions>
 
-        <v-slide-y-transition>
-          <v-card-text v-show="education.show">
-            <v-chip v-for="(course, i) in education.courses" :key="i">
-              {{ course }}
-            </v-chip>
-          </v-card-text>
-        </v-slide-y-transition>
-      </v-card>
-      <template v-slot:opposite>
-        <span class="font-weight-bold" v-text="education.year"></span>
-      </template>
-    </v-timeline-item>
-  </v-timeline>
+            <v-slide-y-transition>
+              <v-card-text v-show="education.show">
+                <v-chip v-for="(course, i) in education.courses" :key="i">
+                  {{ course }}
+                </v-chip>
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          <template v-slot:opposite>
+            <span class="font-weight-bold" v-text="education.year"></span>
+          </template>
+        </v-timeline-item>
+      </v-timeline>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -56,33 +62,33 @@ export default {
           school: 'ENSAE ParisTech',
           degree: 'MSc in Statistics',
           description:
-            'French engineering school specialised in economy and statistics.',
+            'French engineering school specialised in economy 📈 and statistics 📊.',
           year: '2018 - 2020',
           courses: [
-            'Machine learning',
-            'Stochastic process',
-            'Statistics',
-            'Econometry',
-            'Financial economy',
-            'Game theory'
+            'Machine learning 🤖',
+            'Stochastic process 📉',
+            'Statistics 📊',
+            'Econometry 📋',
+            'Financial economy 💹',
+            'Game theory 🃏'
           ],
-          image: require('@/assets/education/ensae_paristech.jpg'),
-          logo: require('@/assets/education/ensae_logo.jpg'),
+          image: require('@/assets/education/ensae.jpg'),
+          logo: require('@/assets/education/ensae_logo.png'),
           show: false
         },
         {
           school: 'Télécom SudParis',
           degree: 'MSc in Computer Science',
           description:
-            'French engineering school specialised in telecommunication and computer science.',
+            'French engineering school specialised in telecommunication 📡 and computer science 💻.',
           year: '2016 - 2018',
           courses: [
-            'Probability',
-            'Scientific calculus',
-            'Statistical learning',
-            'Data stream programming ',
-            'Software engineering',
-            'Relational algebra'
+            'Probability 🎲',
+            'Scientific calculus 🧮',
+            'Statistical learning 📊',
+            'Data stream programming ▶️',
+            'Software engineering ⌨️',
+            'Relational algebra ♾️'
           ],
           image: require('@/assets/education/telecom_sudparis.jpg'),
           logo: require('@/assets/education/tsp_logo.png'),
@@ -92,13 +98,13 @@ export default {
           school: 'Lycée Charlemagne',
           degree: 'French Preparatory Class - MPSI/MP*',
           description:
-            'Paris 4th arrondissement french preparatory class for engineering school entrance exams.',
+            'Paris 4th arrondissement french preparatory class for engineering school entrance exams 📝.',
           year: '2014 - 2016',
           courses: [
-            'Mathematics',
-            'Physics',
-            'Engineering Science',
-            'Chemistry'
+            'Mathematics 🧮',
+            'Physics ⚛️',
+            'Engineering Science 💡',
+            'Chemistry ⚗️'
           ],
           image: require('@/assets/education/lycee_charlemagne.jpg'),
           logo: require('@/assets/education/charlemagne_logo.png'),
@@ -107,15 +113,15 @@ export default {
         {
           school: 'Lycée Charlemagne',
           degree: 'A-Level - French Scientific Baccalauréat',
-          description: 'Paris 4th arrondissement secondary school.',
+          description: 'Paris 4th arrondissement secondary school 📖.',
           year: '2011 - 2014',
           courses: [
-            'Mathematics',
-            'Physics',
-            'Biology',
-            'Philosophy',
-            'Geography',
-            'History'
+            'Mathematics 🧮',
+            'Physics ⚛️',
+            'Biology 🧫',
+            'Philosophy 🤔',
+            'Geography 🗺️',
+            'History 📜'
           ],
           image: require('@/assets/education/lycee_charlemagne_cours.jpg'),
           logo: require('@/assets/education/charlemagne_logo.png'),
