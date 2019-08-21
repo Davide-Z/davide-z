f9c5d68d8c999ab7ee21dba359e4edb153cd1010
Davide-Z <davide_z@live.fr>
2019-08-21 22:43:34 +0200

    home page : font size changed

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,12 @@
   <v-layout justify-center>
     <v-flex xs12 sm10 md6>
       <v-card flat>
+        <v-card-title primary-title>
+          <div class="display-1">Hi, I'm Davide Z.</div>
+        </v-card-title>
         <blockquote class="blockquote">
-          Hi, I'm Davide Z. This website is under construction 🚧, but feel free
-          to have a look 👀 around.
+          This website is under construction 🚧, but feel free to have a look 👀
+          around.
           <br />
           Its main goal is to present my educational background and some
           projects that I am working or have worked on.
