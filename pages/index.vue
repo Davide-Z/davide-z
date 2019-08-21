30f72bf9a4cd14900315447b6313004d3916462b
Davide-Z <davide_z@live.fr>
2019-08-21 22:27:07 +0200

    home page : under construction message

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,15 @@
 <template>
   <v-layout justify-center>
     <v-flex xs12 sm10 md6>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam luctus
-      feugiat eros vel elementum. Nam non molestie diam. Nunc tempus tempus leo
-      eu blandit. Suspendisse in blandit eros, et dignissim justo. Proin a augue
-      vel dui facilisis varius. Curabitur ullamcorper, ex ut blandit volutpat,
-      lacus turpis condimentum elit, et molestie magna augue ac enim. Maecenas
-      nec blandit leo. Vestibulum et ultrices neque. Nullam nisi orci, volutpat
-      ac volutpat sit amet, commodo vitae elit. Suspendisse viverra velit eu
-      risus varius, a fringilla turpis accumsan. Vestibulum ut libero vitae dui
-      tincidunt mollis. Donec commodo quam eros, ac tempor tellus semper eu.
-      Vestibulum rutrum nisi egestas, pharetra tellus molestie, dictum metus.
-      Proin ullamcorper, elit euismod luctus pellentesque, ante quam bibendum
-      nulla, in vestibulum mauris purus vitae risus. Nulla ultrices, felis ac
-      porttitor tempus, ligula elit efficitur risus, ac malesuada dolor dolor
-      nec augue. Nulla luctus mattis neque non vulputate. Cras scelerisque,
-      ligula at efficitur ultrices, augue dui sodales eros, a molestie lectus
-      velit eget sapien. Vivamus feugiat, arcu ac pretium interdum, mauris enim
-      vestibulum felis, sed accumsan sapien urna id quam.
+      <v-card flat>
+        <blockquote class="blockquote">
+          Hi, I'm Davide Z. This website is under construction 🚧, but feel free
+          to have a look 👀 around.
+          <br />
+          Its main goal is to present my educational background and some
+          projects that I am working or have worked on.
+        </blockquote>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
