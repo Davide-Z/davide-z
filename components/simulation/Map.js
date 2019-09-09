9318f7898e4b3d46e6126958baaa3e3e9e3fc98c
Davide-Z <davide_z@live.fr>
2019-09-09 22:34:09 +0200

    Map: - hexagon tile position adjusted - grass growth added

--- components/simulation/Map.js
+++ components/simulation/Map.js
@@ -1,28 +1,32 @@
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": ".*" }] */
 /* eslint no-console: 0 */
 import * as BABYLON from 'babylonjs'
+import * as MATH from 'mathjs'
 
-const Map = function(width, depth, height, diameter, scene) {
+const Map = function(width, depth, height, diameter, homogeneity, scene) {
   this.scene = scene
+  let ground
+  let grass
 
-  const grassColor = function(rate) {
-    const minColor = BABYLON.Color3.FromHexString('#76552b')
-    const maxColor = BABYLON.Color3.FromHexString('#94ff5e')
+  const groundColor = function(rate) {
+    const minColor = BABYLON.Color3.FromHexString('#d2a62c')
+    const maxColor = BABYLON.Color3.FromHexString('#25d462')
     if (rate === 0) {
-      return BABYLON.Color3.FromHexString('#78edfc')
+      return BABYLON.Color3.FromHexString('#aef4fd')
     } else {
       return BABYLON.Color3.Lerp(minColor, maxColor, rate)
     }
   }
 
   const adjacentIndexes = function(i, j) {
-    const indexes = []
-    indexes.push({ i: i - 1, j: j })
-    for (let m = i; m < i + 2; m++) {
-      for (let n = j - 1; n < j + 2; n++) {
-        indexes.push({ i: m, j: n })
-      }
-    }
+    const indexes = [
+      { i: i - 1 + (j % 2), j: j - 1 },
+      { i: i + (j % 2), j: j - 1 },
+      { i: i - 1, j: j },
+      { i: i + 1, j: j },
+      { i: i - 1 + (j % 2), j: j + 1 },
+      { i: i + (j % 2), j: j + 1 }
+    ]
     return indexes
   }
 
@@ -64,12 +68,16 @@ const Map = function(width, depth, height, diameter, scene) {
     return grid
   }
 
-  const createGrassGrowth = function(grid) {
+  const createGround = function(grid) {
     let row
+    let indexes
     for (let i = 0; i < depth; i++) {
       row = []
       for (let j = 0; j < width; j++) {
-        grid[i][j] *= 1 + Math.atan(Math.random() - 0.5) / Math.PI
+        // transformation according to the homogeneity parameter
+        grid[i][j] *=
+          0.5 +
+          Math.atan((Math.random() - 0.5) / homogeneity / homogeneity) / Math.PI
       }
     }
 
@@ -78,34 +86,54 @@ const Map = function(width, depth, height, diameter, scene) {
     for (let i = 0; i < depth; i++) {
       for (let j = 0; j < width; j++) {
         if (grid[i][j] !== 0) {
-          sum = 0
-          n = 0
-          const indexes = adjacentIndexes(i, j)
+          sum = grid[i][j]
+          n = 1
+          indexes = adjacentIndexes(i, j)
           for (let k = 0; k < indexes.length; k++) {
             sum += gridCopy[indexes[k].i][indexes[k].j]
             n += gridCopy[indexes[k].i][indexes[k].j] !== 0
           }
-          grid[i][j] = sum / n
+          if (n === 1) {
+            // only-child tile removed
+            grid[i][j] = 0
+          } else {
+            grid[i][j] = sum / n
+          }
         }
       }
     }
     return grid
   }
+
+  const createGrass = function(grid) {
+    let gridCopy = grid.map(row => row.slice())
+    gridCopy = gridCopy.map(row => row.map(x => (x > 0 ? x : 0)))
+    return gridCopy
+  }
+
   const renderTile = function(i, j) {
+    const tileMaterial = new BABYLON.StandardMaterial(
+      'tile_material_' + i + '_' + j,
+      scene
+    )
+    tileMaterial.diffuseColor = groundColor(grass[i][j])
+    tileMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
+
     const tile = BABYLON.MeshBuilder.CreateCylinder(
       'tile_' + i + '_' + j,
       {
         height: height,
         diameter: diameter,
         tessellation: 6,
-        faceColors: Array(8).fill(grassColor(ground[i][j]))
+        faceColors: Array(8).fill(groundColor(grass[i][j]))
       },
       scene
     )
     tile.rotation.y = Math.PI / 2
     const x = i + 0.5 * (j % 2)
-    const z = j
+    const z = 0.87 * j
     tile.position = new BABYLON.Vector3(x, 0, z)
+    tile.material = tileMaterial
   }
 
   this.render = function() {
@@ -116,11 +144,18 @@ const Map = function(width, depth, height, diameter, scene) {
     }
   }
 
-  let ground = createWater(3)
-  ground = createGrassGrowth(ground)
+  this.updateGrass = function() {
+    grass = MATH.add(grass, ground.map(x => x.map(y => 0.1 * y))).map(x =>
+      x.map(y => (y <= 1 ? y : 1))
+    )
+  }
+
+  ground = createWater(3)
+  ground = createGround(ground)
+  grass = createGrass(ground)
 
   this.ground = ground
-  console.log(ground)
+  this.grass = grass
 }
 
 export default Map
--- components/simulation/simulationApp.js
+++ components/simulation/simulationApp.js
@@ -6,19 +6,20 @@ import Map from './Map'
 const SimulationApp = function(canvaId) {
   const canvas = document.getElementById(canvaId) // Get the canvas element
   const engine = new BABYLON.Engine(canvas, true) // Generate the BABYLON 3D engine
-
+  let deltaTime = 0
   /** Add the create scene function **/
   const createScene = function() {
     // Create the scene space
     const scene = new BABYLON.Scene(engine)
     scene.clearColor = BABYLON.Color3.FromHexString('#c9feff')
 
-    const width = 40
+    const width = 60
     const depth = 35
     const height = 1
-    const diameter = 1.2
-    const map = new Map(width, depth, height, diameter, scene)
-    map.render()
+    const diameter = 1.1
+    const homogeneity = 0.2
+    scene.map = new Map(width, depth, height, diameter, homogeneity, scene)
+    scene.map.render()
 
     // Add a camera to the scene and attach it to the canvas
     const camera = new BABYLON.ArcRotateCamera(
@@ -26,7 +27,7 @@ const SimulationApp = function(canvaId) {
       0,
       Math.PI / 4,
       Math.sqrt(width * depth),
-      new BABYLON.Vector3((3 * depth) / 5, 0, (5 * width) / 8),
+      new BABYLON.Vector3((3 * depth) / 5, 0, (4 * width) / 9),
       scene
     )
     camera.lowerRadiusLimit = 5
@@ -38,17 +39,12 @@ const SimulationApp = function(canvaId) {
     // const ground = BABYLON.Mesh.CreateGround('ground1', 20, 20, 0, scene)
 
     // Add lights to the scene
-    const light1 = new BABYLON.HemisphericLight(
-      'light1',
-      new BABYLON.Vector3(1, 1, 0),
+    const light = new BABYLON.HemisphericLight(
+      'light',
+      new BABYLON.Vector3(0, 1, 0),
       scene
     )
-    const light2 = new BABYLON.PointLight(
-      'light2',
-      new BABYLON.Vector3(0, 1, -1),
-      scene
-    )
-
+    console.log(scene.map.grass)
     return scene
   }
   /** End of the create scene function **/
@@ -57,6 +53,12 @@ const SimulationApp = function(canvaId) {
 
   // Register a render loop to repeatedly render the scene
   engine.runRenderLoop(function() {
+    deltaTime += engine.getDeltaTime() / 5000
+    if (deltaTime > 1) {
+      deltaTime = 0
+      scene.map.updateGrass()
+      scene.map.render()
+    }
     scene.render()
   })
 
